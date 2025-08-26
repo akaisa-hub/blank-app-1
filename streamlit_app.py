a75f3385328b9c682f36b5c23f39b408ac9e7fb3
akaisa-hub <bayanmiraybayar@gmail.com>
2025-08-26 03:19:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import time
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="🎂 Doğum Günü Kartı 🎂", page_icon="🎉")
+
+st.title("🎉🎂 Mutlu Yıllar Ali! 🎂🎉")
+st.subheader("Senin için özel bir sürpriz Guts- pardon Ali💖")
+
+# Animasyonlu yazı
+message = "İyi ki doğdun, nice mutlu senelere! 🎁✨"
+for i in range(1, len(message)+1):
+    st.write(message[:i])
+    time.sleep(0.05)
+
+st.balloons()
